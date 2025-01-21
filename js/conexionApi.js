bdf3f27c848e619ee1cfa81e7264994881fd8874
Jeremias <jereevivas2603@gmail.com>
2025-01-21 00:50:02 -0300

    Edit conexionApi.js

--- js/conexionApi.js
+++ js/conexionApi.js
@@ -1,39 +1,35 @@
 async function listarProductos() {
-  const conexion = await fetch('../db.json');
+  const conexion = await fetch("http://localhost:3001/products");
 
   const respuesta = conexion.json();
 
   return respuesta;
-
 }
 
-async function crearArticulo(nombre,precio,imagen){
-    const conexion = await fetch('http://localhost:3001/products',{
-        method:'POST',
-        headers:{'Content-Type':'application/json'},
-        body:JSON.stringify({
-            nombre:nombre,
-            precio:precio,
-            imagen:imagen
-        })
-    })
-    const respuesta = await conexion.json();
-    return respuesta;
-
+async function crearArticulo(nombre, precio, imagen) {
+  const conexion = await fetch("http://localhost:3001/products", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      nombre: nombre,
+      precio: precio,
+      imagen: imagen,
+    }),
+  });
+  const respuesta = await conexion.json();
+  return respuesta;
 }
 
 async function eliminarArticulo(id) {
-    const conexion = await fetch(`http://localhost:3001/products/${id}`, {
-        method: 'DELETE',
-    });
-    const respuesta = await conexion.json();
-    return respuesta;
-
+  const conexion = await fetch(`http://localhost:3001/products/${id}`, {
+    method: "DELETE",
+  });
+  const respuesta = await conexion.json();
+  return respuesta;
 }
 
-
-
-
-export const conexionApi={ 
-    listarProductos, crearArticulo, eliminarArticulo
-}
+export const conexionApi = {
+  listarProductos,
+  crearArticulo,
+  eliminarArticulo,
+};
